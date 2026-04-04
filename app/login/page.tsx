52f7b0b7be3a9ba298cc6e565d92d64b3cfbf85a
jeddelich@gmail.com <jeddelich@gmail.com>
2026-04-03 23:59:37 -0400

    feat: create sign up modal

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -13,26 +13,36 @@ interface Inputs {
 }
 
 function Login() {
-  const [login, setLogin] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
+  const [loginModal, setLoginModal] = useState(true);
   const {signIn, signUp} = useAuth() 
   
   const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
+    register: registerSignIn,
+    handleSubmit: handleSubmitSignIn,
+    watch: watchSignIn,
+    reset: resetSignIn,
+    formState: { errors: signInErrors },
   } = useForm<Inputs>();
 
-  const passwordValue = watch("password");
+  const {
+    register: registerSignUp,
+    handleSubmit: handleSubmitSignUp,
+    watch: watchSignUp,
+    reset: resetSignUp,
+    formState: { errors: signUpErrors },
+  } = useForm<Inputs>();
 
-  const onSubmit: SubmitHandler<Inputs> = async ({email, password}) => {
-    if (login) {
-      await signIn(email, password)
-    } else {
-      await signUp(email, password)
-    }
-  }
+  const signInPasswordValue = watchSignIn("password");
+  const signUpPasswordValue = watchSignUp("password");
+
+  const onSignIn: SubmitHandler<Inputs> = async ({ email, password }) => {
+    await signIn(email, password);
+  };
+
+  const onSignUp: SubmitHandler<Inputs> = async ({ email, password }) => {
+    await signUp(email, password);
+  };
 
   return (
     <div className="relative flex h-screen w-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent">
@@ -55,8 +65,10 @@ function Login() {
         height={150}
       />
 
+{
+        loginModal ? (
       <form
-        onSubmit={handleSubmit(onSubmit)}
+        onSubmit={handleSubmitSignIn(onSignIn)}
         className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14"
       >
         <h1 className="text-4xl font-semibold">Sign In</h1>
@@ -66,9 +78,9 @@ function Login() {
               type="email"
               placeholder="Email"
               className="input"
-              {...register("email", { required: true })}
+              {...registerSignIn("email", { required: true })}
             />
-            {errors.email && (
+            {signInErrors.email && (
               <p className="p-1 text-[13px] font-light  text-orange-500">
                 Please enter a valid email.
               </p>
@@ -79,10 +91,10 @@ function Login() {
               type={showPassword ? "text" : "password"}
               placeholder="Password"
               className="input"
-              {...register("password", { required: true })}
+              {...registerSignIn("password", { required: true })}
             />
             {
-              passwordValue && (
+              signInPasswordValue && (
                 showPassword ? (
                   <FaRegEyeSlash className="opacity-75 absolute right-4 top-1/2 transform -translate-y-1/2 cursor-pointer hover:scale-95" onClick={() => setShowPassword(false)}/>
                 ) : (
@@ -90,7 +102,7 @@ function Login() {
                 )
               )
             } 
-            {errors.password && (
+            {signInErrors.password && (
               <p className="p-1 text-[13px] font-light  text-orange-500">
                 Your password must contain between 4 and 60 characters.
               </p>
@@ -99,19 +111,79 @@ function Login() {
         </div>
 
         <button 
+          type="submit"
           className="w-full rounded bg-[#e50914] py-3 font-semibold hover:bg-[#f6121d] cursor-pointer active:scale-95 transition duration-150" 
-          onClick={() => setLogin(true)}
         >
           Sign In
         </button>
 
         <div>
           New to Netflix?
-          <button type="submit" className="text-white hover:underline ml-2 cursor-pointer active:scale-95" onClick={() => setLogin(false)}>
+          <button type="button" className="text-white hover:underline ml-2 cursor-pointer active:scale-95" onClick={() => { setLoginModal(false); resetSignUp(); }}>
             Create an account
           </button>
         </div>
       </form>
+        ) : (
+          <form
+        onSubmit={handleSubmitSignUp(onSignUp)}
+        className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14"
+      >
+        <h1 className="text-4xl font-semibold">Create Your Account</h1>
+        <div className="space-y-4">
+          <label className="inline-block w-full">
+            <input
+              type="email"
+              placeholder="Email"
+              className="input"
+              {...registerSignUp("email", { required: true })}
+            />
+            {signUpErrors.email && (
+              <p className="p-1 text-[13px] font-light  text-orange-500">
+                Please enter a valid email.
+              </p>
+            )}
+          </label>
+          <label className="inline-block w-full relative">
+            <input
+              type={showPassword ? "text" : "password"}
+              placeholder="Password"
+              className="input"
+              {...registerSignUp("password", { required: true })}
+            />
+            {
+              signUpPasswordValue && (
+                showPassword ? (
+                  <FaRegEyeSlash className="opacity-75 absolute right-4 top-1/2 transform -translate-y-1/2 cursor-pointer hover:scale-95" onClick={() => setShowPassword(false)}/>
+                ) : (
+                  <FaRegEye className="opacity-75 absolute right-4 top-1/2 transform -translate-y-1/2 cursor-pointer hover:scale-95" onClick={() => setShowPassword(true)} />
+                )
+              )
+            } 
+            {signUpErrors.password && (
+              <p className="p-1 text-[13px] font-light  text-orange-500">
+                Your password must contain between 4 and 60 characters.
+              </p>
+            )}
+          </label>
+        </div>
+
+        <button 
+         type="submit"
+          className="w-full rounded bg-[#e50914] py-3 font-semibold hover:bg-[#f6121d] cursor-pointer active:scale-95 transition duration-150" 
+        >
+          Sign Up
+        </button>
+
+        <div>
+          Already have an account?
+          <button type="button" className="text-white hover:underline ml-2 cursor-pointer active:scale-95" onClick={() => { setLoginModal(true); resetSignIn(); }}>
+            Sign in
+          </button>
+        </div>
+      </form>
+        )
+        }
     </div>
   );
 }
