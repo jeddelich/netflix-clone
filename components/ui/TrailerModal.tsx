e44c2e15a197c6886226d1f5c35cf8c872741892
jeddelich@gmail.com <jeddelich@gmail.com>
2026-04-08 12:28:20 -0400

    feat: add bottom half of trailer modal with dynamic movie info

--- components/ui/TrailerModal.tsx
+++ components/ui/TrailerModal.tsx
@@ -58,7 +58,7 @@ function TrailerModal() {
       onClose={closeTrailer}
       aria-labelledby="modal-modal-title"
       aria-describedby="modal-modal-description"
-      className="fixed top-7 left-0 right-0 z-50 mx-auto w-full max-w-5xl overflow-hidden rounded-md"
+      className="fixed top-7 left-0 right-0 z-50 mx-auto w-full max-w-5xl overflow-hidden overflow-y-scroll rounded-md"
     >
       <div>
         <button
@@ -107,6 +107,36 @@ function TrailerModal() {
                 <MdVolumeUp className="h-10 w-10 text-white" />
               )}
             </button>
+          </div>
+        </div>
+
+        <div className="flex space-x-16 rounded-b-md bg-[#181818] px-10 py-8">
+          <div className="space-y-6 text-lg">
+            <div className="flex items-center space-x-2 text-sm">
+              <p className="font-semibold text-green-400">{selectedMovie!.vote_average * 10}% Match</p>
+              <p className="font-light">{selectedMovie?.release_date || selectedMovie?.first_air_date}</p>
+              <div className="flex h-4 items-center justify-center rounded border border-white/40 px-1.5 text-xs">HD</div>
+            </div>
+
+            <div className="flex flex-col gap-x-10 gap-y-4 font-light md:flex-row">
+              <p className="w-5/6">{selectedMovie?.overview}</p>
+              <div className="flex flex-col space-y-3 text-sm">
+                <div>
+                  <span className="text-[gray]">Genres: </span>
+                  {genres?.map((genre) => genre.name).join(", ")}
+                </div>
+
+                <div>
+                  <span className="text-[gray]">Original Language: </span>
+                  {selectedMovie?.original_language}
+                </div>
+
+                <div>
+                  <span className="text-[gray]">Total votes: </span>
+                  {selectedMovie?.vote_count}
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
