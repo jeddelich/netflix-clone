7fb5251c9a8735d29b466861a40924bcffa80554
jeddelich@gmail.com <jeddelich@gmail.com>
2026-04-07 23:53:15 -0400

    feat: add custom controls to trailer videos

--- components/ui/TrailerModal.tsx
+++ components/ui/TrailerModal.tsx
@@ -8,6 +8,10 @@ import { Element, Genre } from "@/typings";
 import { baseUrl } from "@/constants/movie";
 import { Modal } from "@mui/material";
 import { IoClose } from "react-icons/io5";
+import { FaPlay } from "react-icons/fa";
+import { PlusIcon } from "@heroicons/react/24/solid";
+import { LuThumbsUp } from "react-icons/lu";
+import { MdVolumeOff, MdVolumeUp } from "react-icons/md";
 
 function TrailerModal() {
   const { selectedMovie, trailerUrl, closeTrailer, isOpen } = useTrailerStore();
@@ -82,6 +86,28 @@ function TrailerModal() {
               }}
             />
           )}
+          <div className="absolute bottom-7 flex w-full items-center justify-between px-10">
+            <div className="flex space-x-4">
+              <button className="flex items-center gap-x-2 rounded bg-white px-8 text-xl font-bold text-black transition hover:bg-[#e6e6e6] cursor-pointer">
+                <FaPlay className="h-4 w-4 text-black md:h-7 md:w-7" />
+                Play
+              </button>
+
+              <button className="h-4 w-4 md:h-10 md:w-10 bg-gray-400/30 rounded-full flex justify-center items-center cursor-pointer border-2 border-gray-500 hover:bg-transparent">
+                <PlusIcon className="w-[80%] h-[80%] text-white" />
+              </button>
+              <button className="h-4 w-4 md:h-10 md:w-10 bg-gray-400/30 rounded-full flex justify-center items-center cursor-pointer border-2 border-gray-500 hover:bg-transparent">
+                <LuThumbsUp className="w-[60%] h-[60%] text-white" />
+              </button>
+            </div>
+            <button className="cursor-pointer" onClick={() => setMuted(!muted)}>
+              {muted ? (
+                <MdVolumeOff className="h-10 w-10 text-white" />
+              ) : (
+                <MdVolumeUp className="h-10 w-10 text-white" />
+              )}
+            </button>
+          </div>
         </div>
       </div>
     </Modal>
