cb2c43bc28b9376a7cd93d8f786533563e5f9207
jeddelich@gmail.com <jeddelich@gmail.com>
2026-04-11 16:23:58 -0400

    feat: add more info button functionality to movie banner

--- components/ui/MovieBanner.tsx
+++ components/ui/MovieBanner.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { baseUrl } from "@/constants/movie";
+import { useTrailerStore } from "@/store/useTrailerStore";
 import { Movie } from "@/typings";
 import { InformationCircleIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
@@ -12,59 +13,70 @@ interface Props {
 }
 
 function MovieBanner({ netflixOriginals }: Props) {
-
   const [movie, setMovie] = useState<Movie | null>(null);
+  const { openTrailer } = useTrailerStore();
 
   useEffect(() => {
     setMovie(
-      netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]
+      netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)],
     );
   }, [netflixOriginals]);
 
   if (!movie) return null;
 
   const movieTitle = movie.title || movie.name || movie.original_name;
+  const mediaType =
+    movie.media_type === "tv" || (!!movie.first_air_date && !movie.release_date)
+      ? "tv"
+      : "movie";
 
   return (
-      <div className="relative w-full h-screen lg:h-[140vh] flex flex-col justify-end overflow-hidden">
-        <div className="absolute top-0 left-0 w-full h-full -z-10">
-          <Image
-            src={`${baseUrl}${movie.backdrop_path || movie.poster_path}`}
-            alt={movieTitle || "Movie Banner"}
-            fill
-            style={{ objectFit: "cover" }}
-            priority
-          />
+    <div className="relative w-full h-screen lg:h-[140vh] flex flex-col justify-end overflow-hidden">
+      <div className="absolute top-0 left-0 w-full h-full -z-10">
+        <Image
+          src={`${baseUrl}${movie.backdrop_path || movie.poster_path}`}
+          alt={movieTitle || "Movie Banner"}
+          fill
+          style={{ objectFit: "cover" }}
+          priority
+        />
 
-          <div
-            className="absolute bottom-0 left-0 w-full h-[70%] pointer-events-none"
-            style={{
-              background:
-                "linear-gradient(to bottom, rgba(20,20,20,0) 0%, #141414 100%)",
-            }}
-          />
-        </div>
+        <div
+          className="absolute bottom-0 left-0 w-full h-[70%] pointer-events-none"
+          style={{
+            background:
+              "linear-gradient(to bottom, rgba(20,20,20,0) 0%, #141414 100%)",
+          }}
+        />
+      </div>
 
-        <div className="absolute top-1/3 sm:top-2/7 md:top-2/5 lg:top-1/3 left-4 lg:left-16 z-10">
-          <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold">
-            {movieTitle}
-          </h1>
-          <p className="mt-2 max-w-xs text-xs sm:max-w-sm sm:text-sm md:max-w-lg md:text-lg lg:max-w-2xl lg:text-xl text-shadow-md">
-            {movie.overview}
-          </p>
+      <div className="absolute top-1/3 sm:top-2/7 md:top-2/5 lg:top-1/3 left-4 lg:left-16 z-10">
+        <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold">
+          {movieTitle}
+        </h1>
+        <p className="mt-2 max-w-xs text-xs sm:max-w-sm sm:text-sm md:max-w-lg md:text-lg lg:max-w-2xl lg:text-xl text-shadow-md">
+          {movie.overview}
+        </p>
 
-          <div className="flex space-x-3 mt-4">
-            <button className="banner__btn bg-white text-black cursor-not-allowed transition hover:opacity-75">
-              <FaPlay className="h-4 w-4 text-black" />
-              Play
-            </button>
-            <button className="banner__btn bg-[gray]/70 flex items-center gap-x-1 transition hover:bg-[gray]/55 cursor-not-allowed" onClick={() => console.log(movie)}>
-              More Info{" "}
-              <InformationCircleIcon className="h-5 w-5" />
-            </button>
-          </div>
+        <div className="flex space-x-3 mt-4">
+          <button className="banner__btn bg-white text-black cursor-not-allowed transition hover:opacity-75">
+            <FaPlay className="h-4 w-4 text-black" />
+            Play
+          </button>
+          <button
+            className="banner__btn bg-[gray]/70 flex items-center gap-x-1 transition hover:bg-[gray]/55 cursor-pointer"
+            onClick={() =>
+              openTrailer(
+                movie,
+                `https://api.themoviedb.org/3/${mediaType}/${movie.id}?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=en-US&append_to_response=videos`,
+              )
+            }
+          >
+            More Info <InformationCircleIcon className="h-5 w-5" />
+          </button>
         </div>
       </div>
+    </div>
   );
 }
 
